2d99b1bc9d8c38de503b364d2afa81a86b4d3a84
Stefano Sandonà <sando92@hotmail.com>
2015-11-17 10:38:27 +0100

    darker

--- GPU/GPU-15-16/cuda/darker.cpp
+++ GPU/GPU-15-16/cuda/darker.cpp
@@ -28,14 +28,16 @@ int main(int argc, char *argv[]) {
 		cerr << "The input must be a color image." << endl;
 		return 1;
 	}
-
+	cout << "A";
 	// Convert the input image to grayscale and make it darker
 	CImg< unsigned char > darkGrayImage = CImg< unsigned char >(inputImage.width(), inputImage.height(), 1, 1);
 
 	r=darkGray(inputImage.width(), inputImage.height(), inputImage.data(), darkGrayImage.data());
 	if(r==1){
+		cout << "ERRR";
 		return 1;
 	}
+	cout << "Good";
 	// Save output
 	darkGrayImage.save(("./" + string(argv[1]) + ".dark.seq.bmp").c_str());
 
