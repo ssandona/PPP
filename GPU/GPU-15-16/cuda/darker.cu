9da23d77ec57774b0d6c87f6692264edd94cb441
Stefano Sandonà <sando92@hotmail.com>
2015-11-17 16:39:42 +0100

    darker

--- GPU/GPU-15-16/cuda/darker.cu
+++ GPU/GPU-15-16/cuda/darker.cu
@@ -70,7 +70,7 @@ int darkGray(const int width, const int height, const unsigned char *inputImage,
 
     // Copy input to device
     memoryTimer.start();
-    if ( (devRetVal = cudaMemcpy(devInputImage, inputImage, pixel_numbers * sizeof(unsigned char), cudaMemcpyHostToDevice)) != cudaSuccess ) {
+    if ( (devRetVal = cudaMemcpy(devInputImage, reinterpret_cast< void *>(inputImage), pixel_numbers * sizeof(unsigned char), cudaMemcpyHostToDevice)) != cudaSuccess ) {
         cerr << "Impossible to copy devA to device." << endl;
         return 1;
     }
