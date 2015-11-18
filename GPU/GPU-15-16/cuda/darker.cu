07fe91db17f220751fe2296deb39e09da5e5c6c8
Stefano Sandonà <sando92@hotmail.com>
2015-11-18 16:25:19 +0100

    remove images

--- GPU/GPU-15-16/cuda/darker.cu
+++ GPU/GPU-15-16/cuda/darker.cu
@@ -31,14 +31,14 @@ __global__ void darkGrayKernel(const int width, const int height, const unsigned
     if(j >= width || i >= height) return;
 
     float grayPix = 0.0f;
-    if(blockIdx.x >= 10) {
+    //if(blockIdx.x >= 10) {
         float r = static_cast< float >(inputImage[(i * width) + j]);
         float g = static_cast< float >(inputImage[(width * height) + (i * width) + j]);
         float b = static_cast< float >(inputImage[(2 * width * height) + (i * width) + j]);
 
         grayPix = ((0.3f * r) + (0.59f * g) + (0.11f * b));
         grayPix = (grayPix * 0.6f) + 0.5f;
-    }
+    //}
     outputImage[(i * width) + j] = static_cast< unsigned char >(grayPix);
 }
 
