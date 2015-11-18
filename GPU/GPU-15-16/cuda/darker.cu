69e1774b82d9c92434ba4fd78c5bc052da3dd826
Stefano Sandonà <sando92@hotmail.com>
2015-11-18 12:38:02 +0100

    darker

--- GPU/GPU-15-16/cuda/darker.cu
+++ GPU/GPU-15-16/cuda/darker.cu
@@ -73,7 +73,7 @@ int darkGray(const int width, const int height, unsigned char *inputImage, unsig
         cerr << "Impossible to allocate device memory for inputImage." << endl;
         return 1;
     }
-    if ( (devRetVal = cudaMalloc(reinterpret_cast< void ** >(&devDarkGrayImage), pixel_numbers * 3 * sizeof(unsigned char))) != cudaSuccess ) {
+    if ( (devRetVal = cudaMalloc(reinterpret_cast< void ** >(&devDarkGrayImage), pixel_numbers * sizeof(unsigned char))) != cudaSuccess ) {
         cerr << "Impossible to allocate device memory for darkGrayImage." << endl;
         return 1;
     }
