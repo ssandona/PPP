12a89e0fab22a5a4091af8e051df1a1f4593a779
Stefano Sandonà <sando92@hotmail.com>
2015-11-24 11:14:49 +0100

    smooth peto

--- GPU/GPU-15-16/cuda/smooth.cu
+++ GPU/GPU-15-16/cuda/smooth.cu
@@ -51,7 +51,7 @@ __global__ void triangularSmoothDKernel(const int width, const int height, const
         smoothPix /= filterSum;
 
 
-        if(i <=1 || j <=1) {
+        if(threadIdx.x == 0 || threadIdx.y == 0) {
             //if(z == 0) {
         		smoothImage[(z * width * height) + (i * width) + j] = static_cast< unsigned char >(smoothPix + 0.5f);
                 //smoothImage[(z * width * height) + (i * width) + j] = static_cast< unsigned char >(0.0f);
