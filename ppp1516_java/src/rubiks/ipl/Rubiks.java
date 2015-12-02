2dea709c2fdc68fef2afe616fd62e0fb633f22fe
Stefano Sandonà <sando92@hotmail.com>
2015-12-02 14:38:02 +0100

    rubik ok boolean before this

--- ppp1516_java/src/rubiks/ipl/Rubiks.java
+++ ppp1516_java/src/rubiks/ipl/Rubiks.java
@@ -26,7 +26,7 @@ public class Rubiks {
 
 
     static IbisCapabilities ibisCapabilities = new IbisCapabilities(
-        IbisCapabilities.ELECTIONS_STRICT, IbisCapabilities.MEMBERSHIP_TOTALLY_ORDERED, IbisCapabilities.TERMINATION);
+        IbisCapabilities.ELECTIONS_STRICT, IbisCapabilities.MEMBERSHIP_TOTALLY_ORDERED);
 
     /*static IbisCapabilities ibisCapabilities = new IbisCapabilities(
         IbisCapabilities.ELECTIONS_STRICT);*/
