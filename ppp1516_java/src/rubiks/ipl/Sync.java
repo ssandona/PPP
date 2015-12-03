238e7e6ec2156e35a548c623b770e99e7ee34346
Stefano Sandonà <sando92@hotmail.com>
2015-12-03 19:37:23 +0100

    rubik new version try workManager & tokenManager peto

---
+++ ppp1516_java/src/rubiks/ipl/Sync.java
@@ -0,0 +1,22 @@
+package rubiks.ipl;
+import ibis.ipl.*;
+
+public class SyncToken {
+    boolean tokenComeBack = false;
+    Token receivedToken;
+
+    public Token waitToken() {
+        while(!tokenComeBack) {
+            this.wait();
+        }
+        tokenComeBack = false;
+    }
+
+    public void arrivedToken(Token t) {
+        receivedToken = t;
+        tokenComeBack = true;
+        notifyAll();
+    }
+}
+
+
