e30d96cdb9b88fc0cd4a2dcd06e6c2dacd948be9
Stefano Sandonà <sando92@hotmail.com>
2015-12-04 13:47:52 +0100

    rubik new version try workManager & tokenManager find peto

--- ppp1516_java/src/rubiks/ipl/Rubiks.java
+++ ppp1516_java/src/rubiks/ipl/Rubiks.java
@@ -155,9 +155,8 @@ public class Rubiks {
                 Cube c = toDo.remove(n);
                 if(c == null) {
                     System.out.println("Ibis[" + myIntIbisId + "] -> AHAHAHA 1, index -> "+n);
-                    return null;
                 }
-                //workToReturn.add(c);
+                workToReturn.add(c);
             } else {
                 int amount = toDo.size() / 2;
                 boolean even = toDo.size() % 2 == 0;
