a937567f2793c55676f87ba5c36a910edebcbbab
Stefano Sandonà <sando92@hotmail.com>
2015-12-04 13:56:07 +0100

    rubik new version try workManager & tokenManager find peto

--- ppp1516_java/src/rubiks/ipl/Rubiks.java
+++ ppp1516_java/src/rubiks/ipl/Rubiks.java
@@ -114,7 +114,7 @@ public class Rubiks {
                         //System.out.println("Ibis[" + myIntIbisId + "] -> no work");
                     } else {
                         r = workReceiver.receive();
-                        System.out.println("Ibis[" + myIntIbisId + "] -> work!!! " + cubes + " cubes");
+                        //System.out.println("Ibis[" + myIntIbisId + "] -> work!!! " + cubes + " cubes");
                         receivedWork = new Cube[cubes];
                         r.readArray(receivedWork);
                         r.finish();
@@ -248,7 +248,7 @@ public class Rubiks {
                 }
 
                 subPoolToSend = subPool.toArray(new Cube[subPool.size()]);
-                System.out.println("Ibis[" + myIntIbisId + "] -> pool to send not empty => " + subPoolToSend.length);
+                //System.out.println("Ibis[" + myIntIbisId + "] -> pool to send not empty => " + subPoolToSend.length);
                 reply.writeInt(subPoolToSend.length);
                 reply.finish();
                 reply = replyPort.newMessage();
