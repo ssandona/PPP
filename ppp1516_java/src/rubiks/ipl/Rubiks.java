96f41feef1c78531841de14c368f64e3768f9859
Stefano Sandonà <sando92@hotmail.com>
2015-12-07 09:09:30 +0100

    try to evaluate syn cost

---
+++ ppp1516_java/src/rubiks/ipl/Rubiks.java
@@ -0,0 +1,29 @@
+package rubiks.ipl;
+
+import ibis.ipl.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.IOException;
+
+/**
+ * Solver for rubik's cube puzzle.
+ *
+ * @author Niels Drost, Timo van Kessel
+ *
+ */
+//public class Rubiks implements RegistryEventHandler {
+public class Rubiks {
+
+    
+
+    public static void main(String[] arguments) {7
+        int i;
+        Object lock = new Object();
+        int sum=0;
+        for(i=0;i<1000000;i++){
+            sum+=i;
+        }
+    }
+
+
+}
