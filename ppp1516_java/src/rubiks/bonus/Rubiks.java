729c60dc8ac87563652cf3e8be51c89972ff4da5
Stefano Sandonà <sando92@hotmail.com>
2016-01-11 19:13:40 +0100

    try thread 2

--- ppp1516_java/src/rubiks/bonus/Rubiks.java
+++ ppp1516_java/src/rubiks/bonus/Rubiks.java
@@ -202,7 +202,7 @@ public class Rubiks {
         Cube cube;
         boolean end = false;
 
-        int THREAD_NUMBER = 4;
+        int THREAD_NUMBER = 12;
         int j;
         int[] cubes_per_thread = new int[THREAD_NUMBER];
         int avarage_cubes_per_thread = toDo.size() / THREAD_NUMBER;
