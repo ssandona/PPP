3a4bae92edf3f0b6dcbe92991e3521022dea0662
Stefano Sandonà <sando92@hotmail.com>
2016-01-19 10:00:17 +0100

    darker try

--- GPU/GPU-15-16/cuda/darker_try.cu
+++ GPU/GPU-15-16/cuda/darker_try.cu
@@ -14,12 +14,13 @@ const unsigned int nrThreads = 256;
 
 __global__ void kernel(const int width, const int height, const unsigned char * inputImage, unsigned char * outputDarkGrayImage) {
 	//unsigned int item = (blockIdx.x * blockDim.x + threadIdx.x)+(blockIdx.y * width);
-
 	unsigned int i;
-    /*unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
+	/*unsigned int i = ;
+    unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
     unsigned int globalIdx=(blockIdx.x * blockDim.x + threadIdx.x)+(threadIdx.y * width);*/
 
-    for(i = ((blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x) + threadIdx.x; i < width * height; i += (gridDim.x * blockDim.x) *		float grayPix = 0.0f;
+	for(i = ((blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x) + threadIdx.x; i < width * height; i += (gridDim.x * blockDim.x) * (gridDim.y * blockDim.y)) {
+		float grayPix = 0.0f;
 		float r = static_cast< float >(inputImage[i]);
 		float g = static_cast< float >(inputImage[(width * height) + i]);
 		float b = static_cast< float >(inputImage[(2 * width * height) + i]);
