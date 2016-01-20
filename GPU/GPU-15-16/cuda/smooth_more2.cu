dd6dcaaf0e2675c659ec55ad01f3372622febc51
Stefano Sandonà <sando92@hotmail.com>
2016-01-20 10:33:36 +0100

    smooth try

--- GPU/GPU-15-16/cuda/smooth_more2.cu
+++ GPU/GPU-15-16/cuda/smooth_more2.cu
@@ -38,8 +38,12 @@ __global__ void triangularSmoothDKernel(const int width, const int height, const
     if(j < width && i < height) {
 
         //coordinates of the top left pixel for the localImagePortion
-        int topLeftPxI = (i - threadIdx.y) - 2;
-        int topLeftPxJ = (j - threadIdx.x) - 2;
+        //int topLeftPxI = (i - threadIdx.y) - 2;
+        //int topLeftPxJ = (j - threadIdx.x) - 2;
+
+        //coordinates of the top left pixel for the localImagePortion
+        int topLeftPxI = (blockIdx.y * blockDim.y) - 2;
+        int topLeftPxJ = (blockIdx.x * blockDim.x) - 2;
 
         //coordinates of the first pixel to copy into the localImagePortion
         int pxAI = topLeftPxI + (inBlockIdx / 20);
