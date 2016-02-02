603bba961e32f6120f8240d9e7ef5590078f412d
Stefano Sandonà <sando92@hotmail.com>
2016-02-02 16:18:12 +0100

    smooth more

--- GPU/GPU-15-16/cuda/smooth_more2.cu
+++ GPU/GPU-15-16/cuda/smooth_more2.cu
@@ -122,8 +122,8 @@ __global__ void triangularSmoothDKernel(const int width, const int height, const
                 }
 
                 smoothPix /= filterSum;
-                //smoothImage[(z * width * height) + (i * width) + j] = static_cast< unsigned char >(smoothPix + 0.5f);
-                smoothImage[(z * width * height) + (i * width) + j] =localImagePortion[(z * 36 * 20) + (inLocalPortionI*36) + inLocalPortionJ];
+                smoothImage[(z * width * height) + (i * width) + j] = static_cast< unsigned char >(smoothPix + 0.5f);
+                //smoothImage[(z * width * height) + (i * width) + j] =localImagePortion[(z * 36 * 20) + (inLocalPortionI*36) + inLocalPortionJ];
             }
         }
 
